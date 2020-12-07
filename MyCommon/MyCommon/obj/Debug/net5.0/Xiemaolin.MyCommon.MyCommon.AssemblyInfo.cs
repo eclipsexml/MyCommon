ca943cb1051e2cbcc5de5cc77450dfa382726420
xml <393020091@qq.com>
2020-12-07 23:17:52 +0800

    123

--- MyCommon/MyCommon/obj/Debug/net5.0/Xiemaolin.MyCommon.MyCommon.AssemblyInfo.cs
+++ MyCommon/MyCommon/obj/Debug/net5.0/Xiemaolin.MyCommon.MyCommon.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Xiemaolin.MyCommon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.1")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.2.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Xiemaolin.MyCommon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Xiemaolin.MyCommon")]
-[assembly: System.Reflection.AssemblyVersionAttribute("0.0.1.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("0.0.2.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
